2e6558d2bd5b0acd6cb2299feaa3cb2c8f97cb1a
ACoxWebDevelopment <144549496+ACoxWebDevelopment@users.noreply.github.com>
2026-05-01 20:54:19 -0600

    Delete week1 directory

--- week1/scripts/navigation.js
+++
@@ -1,8 +0,0 @@
-const navbutton = document.querySelector('#nav-button');
-// toggle the show class on and off
-const navbar = document.querySelector('#nav-bar');
-navbutton.addEventListener ('click' , () =>
-{
-    navbutton.classList.toggle('show');
-    navbar.classList.toggle('show');
-} );
