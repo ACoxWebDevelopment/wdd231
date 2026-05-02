5513ae453dd6969e22e8b9762041a43093f736ca
ACoxWebDevelopment <144549496+ACoxWebDevelopment@users.noreply.github.com>
2026-05-01 20:54:59 -0600

    Delete scripts directory

--- scripts/navigation.js
+++
@@ -1,6 +0,0 @@
-const navbutton = document.querySelector ("#nav-button");
-const navmenu = document.querySelector ("#nav-menu");
-navbutton.addEventListener("click", () => {navbutton.classList.toggle("show");
-    navmenu.classList.toggle("show");
-} )
-
