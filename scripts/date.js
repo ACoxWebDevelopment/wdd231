8424d483b0856cbaa0d1c2c246bdb3da18342194
Allan Cox <Allan.L.Coxrr@gmail.com>
2026-06-02 17:35:16 -0600

    siteplan page

--- scripts/date.js
+++ scripts/date.js
@@ -1,10 +1,16 @@
 const copy= document.querySelector("#copyYear");
 const mod = document.querySelector("#modified");
 const today = new Date();
-
+const joinTime = document.getElementById("#timestamp")
 
 copy.innerHTML = `\u00A9 ${today.getFullYear()}`; //get current year and place in copy variable
 mod.innerHTML = `Last Modified ${document.lastModified}`;
 
+function joinTimestamp(){
+  if(joinTime)
+    {
+      joinTime.value = new Date().toISOString();
+    }
 
-  document.getElementById("timestamp").value = new Date().toISOString();
+}
+joinTimestamp();
