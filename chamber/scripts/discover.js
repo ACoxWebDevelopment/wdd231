550afed750dcdcc9fd528e77c417f579fecbc5c2
Allan Cox <Allan.L.Coxrr@gmail.com>
2026-06-06 15:14:30 -0600

    added last visit logic

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,5 +1,35 @@
 import { places } from "../data/Discover.mjs";
 const card = document.querySelector(".card");
+const visit = Date.now() ;
+// milliseconds in a day 1000ms/sec*60sec/min*60min/hr*24hr/day
+const day = 86400000;
+const lastVisit = Number(localStorage.getItem("visited"));
+let visitMessage = "";
+
+if (lastVisit)
+{
+    const numDays = (visit - lastVisit) / day;
+
+    if (numDays < 1)
+    {
+        visitMessage="Back so soon! Awesome!";
+    }
+    else
+    {
+        visitMessage=`It has been ${Math.floor(numDays)} days since your last visit`;
+    }
+}
+else
+{
+    visitMessage="Welcome";
+}
+document.querySelector("#visitInfo").textContent = visitMessage;
+
+localStorage.setItem("visited", visit);
+
+
+
+
 places.forEach(element => {
 
     let cardDetail = document.createElement ("div");
@@ -13,6 +43,7 @@ places.forEach(element => {
     cardDetail.className = ("placeCard");
     title.textContent = element.title;
     photo.src = element.image;
+    photo.loading = ("lazy");
     address.textContent = element.address;
     description.textContent = element.description;
     learnMore.textContent = "learn more";
