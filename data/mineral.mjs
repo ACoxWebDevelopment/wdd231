31648dee78afb6227de25eb44cdc0190a0d247f7
Allan Cox <Allan.L.Coxrr@gmail.com>
2026-06-12 11:27:57 -0600

    home page creation

---
+++ data/mineral.mjs
@@ -0,0 +1,34 @@
+
+export const minerals = 
+[
+    {
+        "name": "Sapphire",
+        "photo": "images/sapphire.webp",
+        "description": "Sapphire is one of the two gem-varieties of corundum, the other being ruby (defined as corundum in a shade of red). Although blue is the best-known sapphire color, it occurs in other colors, including gray and black, and can also be colorless. A pinkish orange variety of sapphire is called padparadscha."
+            },
+    {
+        "name": "Agate",
+        "photo": "images/agate.webp",
+        "description": "Agate stones are characterized by alternating bands of different colored chalcedony and often contain some visible quartz crystals. They are common in nature and can be found globally in a large number of different varieties."
+    },
+    {
+        "name": "Garnet",
+        "photo": "images/garnet.webp",
+        "description": "Garnet minerals, while sharing similar physical and crystallographic properties, exhibit a wide range of chemical compositions, defining distinct species. These species fall into two primary solid solution series: the pyralspite series (pyrope, almandine, spessartine), with the general formula [Mg,Fe,Mn]3Al2(SiO4)3; and the ugrandite series (uvarovite, grossular, andradite), with the general formula Ca3[Cr,Al,Fe]2(SiO4)3. Notable varieties of grossular include hessonite and tsavorite"
+    },
+    {
+        "name": "Chalcedony",
+        "photo": "images/chalcedony.webp",     
+        "description": "Chalcedony[a] is an aggregate of cryptocrystalline or microcrystalline silica, or in simpler terms, a mass of individual silica crystals that are too small to be seen without magnification. It is composed primarily of quartz, with minor amounts of intergrown moganite."
+    },
+    {
+        "name": "Rhodochrosite",
+        "photo": "images/rhodochrosite.webp",
+        "description": "Rhodochrosite is a highly sought-after manganese carbonate mineral known for its striking pink to vibrant red hues. Often called Inca Rose, it is celebrated as an ornamental stone, a rare faceted gemstone, and a popular crystal for holistic healing and metaphysical practices."
+    },
+    {
+        "name": "Topaz",
+        "photo": "images/topaz.webp",
+        "description": "Topaz is a silicate mineral made of aluminum and fluorine with the chemical formula Al2SiO4(F, OH)2. It is used as a gemstone in jewelry and other adornments. Common topaz in its natural state is colorless, though trace element impurities can make it pale blue or golden-brown to yellow-orange.[7] Topaz is often treated with heat or radiation to make it a deep blue, reddish-orange, pale green, pink, or purple."
+    }
+]
---
+++ scripts/gallery.js
@@ -0,0 +1,42 @@
+import {minerals} from "/data/mineral.mjs";
+
+const mineral= document.querySelector("#rockGallery");
+const mineralDetails = document.querySelector(".mineralDetails");
+
+minerals.forEach(element => {
+    let rock = document.createElement("p");
+    let rockPic = document.createElement("img");
+    let learnMore = document.createElement("button");
+    let rockCard = document.createElement("div");
+    let details = document.createElement("p");
+    rockCard.className = "rockCard";
+    learnMore.textContent = "Learn More";
+    learnMore.className = ("mineralDetailsButton");
+    rockPic.src = element.photo;
+    rock.textContent=element.name;
+    details.textContent = element.description
+    details.className = "specificMineral";
+    rockCard.appendChild(rock);
+    rockCard.appendChild(rockPic);
+    rockCard.appendChild(details);
+    rockCard.appendChild(learnMore);
+    mineral.appendChild(rockCard);
+
+    learnMore.addEventListener("click",() => {
+        mineralDetails.showModal();
+        let modalName=document.querySelector(".modalName");
+        let modalDescription=document.querySelector(".modalDescription");
+        modalName.textContent = element.name;
+        modalDescription.textContent = element.description;
+        mineralDetails.appendChild(modalName);
+        mineralDetails.appendChild(modalDescription);
+});
+    let closeButton = document.querySelector(".closeButton");
+    closeButton.addEventListener("click",() => 
+    {
+        mineralDetails.close();
+    })
+});
+
+
+
